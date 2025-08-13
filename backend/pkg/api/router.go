d8b80599e436a7672f3a75191fc03de82add97d3
kunal <kunal22102005@gmail.com>
2025-08-13 15:36:27 +0530

    remove logging

--- backend/pkg/api/router.go
+++ backend/pkg/api/router.go
@@ -3,20 +3,12 @@ package api
 import (
 	"MVC/pkg/middleware"
 	"MVC/pkg/utils"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func InitRouter() *mux.Router {
 	router := mux.NewRouter()
-
-	router.Use(func(handler http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Print(r.URL)
-			handler.ServeHTTP(w, r)
-		})
-	})
 
 	router.Use(utils.AddJSONHeaders)
 	router.Use(middleware.CORSMiddleware)
