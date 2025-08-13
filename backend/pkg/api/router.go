4ca92f8124f4234b76dd2364c476bfb3edee1210
kunal <kunal22102005@gmail.com>
2025-08-13 22:16:04 +0530

    --amend

--- backend/pkg/api/router.go
+++ backend/pkg/api/router.go
@@ -3,12 +3,8 @@ package api
 import (
 	"MVC/pkg/middleware"
 	"MVC/pkg/utils"
-	"fmt"
 	"github.com/gorilla/mux"
-	"io/fs"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 func InitRouter() *mux.Router {
@@ -26,24 +22,6 @@ func InitRouter() *mux.Router {
 	initSubordersRoutes(router)
 	initSingleOrderRoutes(router)
 	initMultipleOrderRoutes(router)
-
-	assetDir := "./assets"
-	info, err := os.Stat(assetDir)
-	if os.IsNotExist(err) {
-		fmt.Println("Assets folder not found!")
-	}
-	if !info.IsDir() {
-		fmt.Println("Assets path is not a directory!")
-	} else {
-		fmt.Println("Assets contents:")
-		filepath.WalkDir(assetDir, func(path string, d fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-			fmt.Println(" -", path)
-			return nil
-		})
-	}
 
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
