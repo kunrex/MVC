e50f42e4209d34b2b94e0cdaa8a9d5d0f45073a1
kunal <kunal22102005@gmail.com>
2025-08-17 01:09:20 +0530

    add dependency

--- backend/pkg/database/db.go
+++ backend/pkg/database/db.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"time"
 )
 
