61885fca5c2b5f6837c5c35d1684357069859cfc
kunal <kunal22102005@gmail.com>
2025-08-17 16:11:52 +0530

    update config path

--- backend/pkg/config/config.go
+++ backend/pkg/config/config.go
@@ -8,15 +8,15 @@ import (
 )
 
 func InitConfig() *types.Config {
-	contents, err := os.ReadFile("./config.json")
+	contents, err := os.ReadFile("./.config.json")
 	if err != nil {
-		log.Fatalf("error reading config.json: %v", err.Error())
+		log.Fatalf("error reading .config.json: %v", err.Error())
 	}
 
 	var config types.Config
 	err = json.Unmarshal(contents, &config)
 	if err != nil {
-		log.Fatalf("error reading config.json: %v", err.Error())
+		log.Fatalf("error reading .config.json: %v", err.Error())
 	}
 
 	return &config
